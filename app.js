3895b8cdc9e3898ec79ae1b28302d348b0cdfa6b
Diego Chavez <dalthon1988@gmail.com>
2023-09-28 13:01:13 -0300

    segunda entrega correjida

--- app.js
+++ app.js
@@ -1,131 +1,149 @@
-import{promises as fs } from 'fs'
+import fs from 'fs/promises';
 
-const RUTA_ARCHIVO = './datos.txt'
+const RUTA_ARCHIVO = './datos.JSON';
 
 class ProductManager {
-    constructor(path) {
-      this.path = path
-      this.products = [];
-    }
-  
-    static incrementarID(){
-      if(this.idIn) {
-        this.idIn++
-      } else {
-        this.idIn=1
-      }
-      return this.idIn
-    }
-  
-    // Método para agregar un nuevo producto se agrega promesas..
-    async addProduct(product) {
-      
-      const products = await fs.readFile(this.path, 'utf-8')
-      const Item = JSON.parse(products)
-      product.id =ProductManager.incrementarID()
-      Item.push(product)
-  
-      if (!product.code ||
-        !product.price ||
-        product.price <= 0 ||
-        product.stock < 0) {
-        return "Product Inavild";
+  constructor(path) {
+    this.path = path;
+  }
 
+  static idIn = 0;
 
-      } else if (
-        this.products.find((producto) => producto.code == product.code)
-      ) {
+  static incrementarID() {
+    if (this.idIn) {
+      this.idIn++;
+    } else {
+      this.idIn = 1;
+    }
+    return this.idIn;
+  }
+
+  async addProduct(product) {
+    try {
+      const products = await fs.readFile(this.path, 'utf-8');
+      const parsedProducts = JSON.parse(products); // Corrección aquí
+  
+      product.id = ProductManager.incrementarID();
+  
+      if (!product.code || !product.price || product.price <= 0 || product.stock < 0) {
+        return "Product Invalid";
+      } else if (parsedProducts.find((producto) => producto.code === product.code)) {
         return "Duplicate product";
       }
-      try {
+  
+      parsedProducts.push(product);
+  
+      await fs.writeFile(this.path, JSON.stringify(parsedProducts));
+      return "The product was added successfully";
+    } catch (error) {
+      console.log(error);
+      return "Error adding product";
+    }
+  }
+  async getProducts() {
+    try {
+      const products = await fs.readFile(this.path, 'utf-8');
+      const parsedProducts = JSON.parse(products);
+      return parsedProducts;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      const products = await fs.readFile(this.path, 'utf-8');
+      const Item = JSON.parse(products);
+      const foundProduct = Item.find((item) => item.id === parseInt(id));
+      if (foundProduct) {
+        return foundProduct;
+      } else {
+        return "Product not found";
+      }
+    } catch (error) {
+      console.log(error);
+      return "Error retrieving product";
+    }
+  }
+
+  async updateProduct(id, { title, description, price, thumbnail, code, stock }) {
+    try {
+      const products = await fs.readFile(this.path, 'utf-8');
+      const Item = JSON.parse(products);
+      const index = Item.findIndex((item) => item.id === parseInt(id));
+      if (index !== -1) {
+        Item[index].title = title;
+        Item[index].description = description;
+        Item[index].price = price;
+        Item[index].thumbnail = thumbnail;
+        Item[index].code = code;
+        Item[index].stock = stock;
         await fs.writeFile(this.path, JSON.stringify(Item));
-      } catch (error) {
-        console.log(error);
-      }
-      return "The product was added successfully";}
-    
-  
-    // Método para obtener todos los productos agregando Promesas 
-    async getProducts() {
-      try{
-        const products = await fs.readFile(this.path,'utf-8')
-        const Item = JSON.parse(products)
-        return Item
-      }catch (error){
-        console.log(error);
-        return[];
-      }
-      
-    }
-  
-    // Método para obtener un producto por su ID
-   async getProductById() {
-      const product = this.products.find((producto) => producto.id === id);
-  
-      if (!product) {
-        return "Producto no encontrado.";
+        return "Updated product";
       } else {
-        return product;
+        return "Product not found";
       }
-    }
-  
-    // Método para actualizar un producto por su ID
-    updateProductById(id, updatedProduct) {
-      const index = this.products.findIndex((producto) => producto.id === id);
-  
-      if (index === -1) {
-        return "Producto no encontrado.";
-      } else {
-        this.products[index] = {
-          ...this.products[index],
-          ...updatedProduct,
-        };
-        return "Producto actualizado con éxito.";
-      }
-    }
-  
-    // Método para eliminar un producto por su ID
-    deleteProductById(id) {
-      const index = this.products.findIndex((producto) => producto.id === id);
-  
-      if (index === -1) {
-        return "Producto no encontrado.";
-      } else {
-        this.products.splice(index, 1);
-        return "Producto eliminado con éxito.";
-      }
+    } catch (error) {
+      console.log(error);
+      return "Error updating product";
     }
   }
-  
-  const productos = new ProductManager();
-  
-  // Primera llamada = arreglo vacío
-  console.log(productos.getProducts());
-  
-  try {
-    // Agrego productos
-    console.log(productos.addProduct("hamburguesa1", "hamburguesa con queso1", 1500, "imagen1", "burger123", 15));
-    console.log(productos.addProduct("hamburguesa2", "hamburguesa con queso2", 1500, "imagen2", "burger1234", 15));
-  
-    // Prueba de código repetido
-    console.log(productos.addProduct("hamburguesa2", "hamburguesa con queso2", 1500, "imagen2", "burger1234", 15));
-  
-    console.log(productos.getProductById(4));
-  
-    // Actualizar un producto
-    console.log(productos.updateProductById(1, {
-      title: "Nueva Hamburguesa",
-      description: "Nueva descripción",
-      price: 2000,
-    }));
-  
-    // Eliminar un producto
-    console.log(productos.deleteProductById(2));
-  
-  } catch (error) {
-    console.error(error.message);
+
+  async deleteProduct(id) {
+    try {
+      const products = await fs.readFile(this.path, 'utf-8');
+      const parsedProducts = JSON.parse(products);
+      const filteredItem = parsedProducts.filter((item) => item.id !== parseInt(id));
+      if (filteredItem.length !== parsedProducts.length) {
+        await fs.writeFile(this.path, JSON.stringify(filteredItem));
+        return "Product was removed";
+      } else {
+        return "Product not found";
+      }
+    } catch (error) {
+      console.log(error);
+      return "Error deleting product";
+    }
   }
-  
-  // Mostrar todos los productos después de las operaciones
-  console.log(productos.getProducts());
-  
+}
+
+class Product {
+  constructor(
+    title = "",
+    description = "",
+    price = 0,
+    thumbnail = "",
+    code = "",
+    stock = 0
+  ) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+  }
+}
+
+const productManager = new ProductManager(RUTA_ARCHIVO);
+
+// muestra los productos
+productManager.getProducts().then((prods) => console.log(prods));
+
+// Agregar Producto
+const newProduct = new Product(
+  "asdasd",
+  "Por asdasdasdKilo",
+  1200,
+  "",
+  "U202222001",
+  20000
+);
+
+productManager.addProduct(newProduct).then((result) => {
+  console.log(result);
+});
+
+// Borrar producto
+//productManager.deleteProduct(1).then((result) => console.log(result));
